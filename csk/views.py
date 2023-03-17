69e9a33726902adf3b45aefb1635f9b0d8b6ab7a
shivam5799 <115656331+shivam5799@users.noreply.github.com>
2023-03-17 07:57:01 +0530

    first commit

---
+++ csk/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def dhoni(request):
+    return render(request,'dhoni.html')
---
+++ rcb/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+
+def virat(request):
+    return render(request,'virat.html')
